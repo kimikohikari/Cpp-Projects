873057ae321f2e90ec619e3f3cc095dcaff52d46
kimikohikari <koralad163@gmail.com>
2025-07-16 13:07:25 +0400

    30 Days Challenge

---
+++ Day1/EasyCalc/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(){ 
+    int a, b;
+    std::cout << "Return numbers"<< std::endl;
+    std::cin >> a >> b;
+    std::cout << "Sum: " << a+b << std::endl << "Subtraction: " << a-b << std::endl << "Multiplication: " << a*b << std::endl << "Division: " << a/b ;
+}
